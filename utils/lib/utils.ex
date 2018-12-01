577fcc093a344fb11543e430445e172b42dc1e79
Per Rovegård <per@rovegard.se>
2018-12-01 23:28:04 +0100

    Adding specs for some Utils functions

--- utils/lib/utils.ex
+++ utils/lib/utils.ex
@@ -1,11 +1,13 @@
 defmodule Utils do
 
+  @spec readlines(String.t()) :: [String.t()]
   def readlines(file) do
     File.stream!(file)
       |> Stream.map(&String.trim_trailing/1)
       |> Enum.to_list
   end
 
+  @spec readlines_as_integers(String.t()) :: [integer]
   def readlines_as_integers(file) do
     Utils.readlines(file)
       |> Enum.map(&String.to_integer/1)
