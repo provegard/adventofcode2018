cb7512d17361c11e8bd197e02a6a920ddd868855
Per Rovegård <per@rovegard.se>
2018-12-01 23:20:30 +0100

    Add Utils.to_tuples_via_regex

--- utils/lib/utils.ex
+++ utils/lib/utils.ex
@@ -10,4 +10,13 @@ defmodule Utils do
     Utils.readlines(file)
       |> Enum.map(&String.to_integer/1)
   end
+
+  def to_tuples_via_regex(enumerable, regex) do
+    enumerable
+      |> Enum.map(fn x ->
+        # First element in list is full match, skip that
+        captures = tl Regex.run(regex, x)
+        List.to_tuple captures
+      end)
+  end
 end
--- utils/test/utils_test.exs
+++ utils/test/utils_test.exs
@@ -11,4 +11,8 @@ defmodule UtilsTest do
     lines = Utils.readlines_as_integers("test/lines.txt")
     assert lines == [1, 2, 3]
   end
+
+  test "to_tuples_via_regex" do
+    assert Utils.to_tuples_via_regex(["a,c", "d,e"], ~r/(.*),(.*)/) == [{"a", "c"}, {"d", "e"}]
+  end
 end
