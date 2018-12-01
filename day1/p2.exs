fd5b6aae6c54d83d583c50614746140cdc5f715c
Per Rovegård <per@rovegard.se>
2018-12-01 13:28:16 +0100

    Day 1, part 2, add comments

--- day1/p2.exs
+++ day1/p2.exs
@@ -5,7 +5,13 @@ nums = Files.readlines_as_numbers("input")
 # ** (ArgumentError) cannot cycle over empty enumerable
 inf = Stream.cycle(nums |> Enum.to_list)
 
+# Accumulate the sum of elements (frequencies)
 accumulated = inf |> Stream.transform(0, fn i, acc -> {[acc], acc + i} end)
+
+# Stop the stream as soon as we find the first duplicate number. We use a transform
+# for this, with a MapSet as the accumulator. We don't halt when we see the first
+# duplicate, because then it won't be emitted. Instead we do one more "round" so that
+# the final stream ends with the first duplicate number.
 until_dup = accumulated |> Stream.transform(MapSet.new(), fn i, set ->
   if set === :halt do
     {:halt, nil}
@@ -20,5 +26,8 @@ until_dup = accumulated |> Stream.transform(MapSet.new(), fn i, set ->
   end
 end)
 
+# Enum.take with negative number takes from the end
 first_dup = Enum.take(until_dup, -1)
+
+# Print the head of the result (which is a list of 1 element)
 IO.inspect hd first_dup
