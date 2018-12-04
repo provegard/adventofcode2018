a8fa4da47ee67c6afa66f0cd855d75c5f7a4be56
Per Rovegård <per@rovegard.se>
2018-12-04 07:44:22 +0100

    Simplifying day 3

--- day3/lib/day3.ex
+++ day3/lib/day3.ex
@@ -2,17 +2,18 @@ defmodule Day3 do
 
   def part1(patches) do
     # Expand all patch coordinates, {x, y}
-    patch_coords = patches |> Enum.flat_map(fn patch ->
-      {_, left, top, width, height} = patch
-      coords(left, top, width, height)
-    end)
+    all_coords = patches |> Enum.flat_map(&patch_coords/1)
 
     # Group by coordinate and count groups where a coordinate occurs more than one time.
     # The coordinate of such a group is overlapped by more than one patch.
-    patch_coords
-      |> Enum.group_by(fn c -> c end)
+    all_coords
+      |> Enum.group_by(fn {_, x, y} -> {x, y} end)
       |> Enum.filter(fn {_, v} -> length(v) > 1 end)
       |> length
+  end
+
+  defp patch_coords({id, left, top, width, height}) do
+    coords(left, top, width, height) |> Enum.map(fn {x, y} -> {id, x, y} end)
   end
 
   defp coords(left, top, width, height) do
@@ -20,24 +21,22 @@ defmodule Day3 do
   end
 
   def part2(patches) do
-    new_grid = Enum.reduce(patches, %{}, fn patch, grid ->
-      {patch_id, left, top, width, height} = patch
-      patch_coords = coords(left, top, width, height)
-      Enum.reduce(patch_coords, grid, fn coord, g ->
-        # Each cell holds a list of patch_ids
-        pids = g[coord]
-        new_value = if pids == nil, do: [patch_id], else: pids ++ [patch_id]
-        Map.put(g, coord, new_value)
-      end)
-    end)
+    all_patch_ids = patches |> Enum.map(fn p -> elem(p, 0) end) |> MapSet.new()
 
-    all_patch_ids = patches |> Enum.map(fn p -> elem(p, 0) end) |> MapSet.new
-    overlapping_ids = new_grid
+    # Expand ID + patch coordinates, {id, x, y}
+    all_coords_with_id = patches |> Enum.flat_map(&patch_coords/1)
+
+    # Group by coordinate and find patch IDs that occur in cells where patches
+    # overlap.
+    overlapping_patch_ids = all_coords_with_id
+      |> Enum.group_by(fn {_, x, y} -> {x, y} end, fn {id, _, _} -> id end)
       |> Enum.filter(fn {_, v} -> length(v) > 1 end)
       |> Enum.flat_map(fn {_, v} -> v end)
-      |> MapSet.new
-    standalone_patch_ids = MapSet.difference(all_patch_ids, overlapping_ids)
-    MapSet.to_list(standalone_patch_ids)
+      |> MapSet.new()
+
+    # Remove overlapping patch IDs from all; the ones left don't overlap.
+    MapSet.difference(all_patch_ids, overlapping_patch_ids)
+      |> MapSet.to_list()
   end
 
 end
