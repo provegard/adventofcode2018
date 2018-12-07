316fcdb619c4fe3e412350159ed599628fbb8120
Per Rovegård <per@rovegard.se>
2018-12-07 13:01:33 +0100

    Day 7, part 2, ugly as hell

--- day7/lib/day7.ex
+++ day7/lib/day7.ex
@@ -3,15 +3,138 @@ defmodule Day7 do
     # Each tuple is an edge {from, to}
     edges = instructions
       |> parse_instructions()
-      #|> Enum.map(fn {f, t} -> %{ }} end)
-    #graph = %Utils.Graph{edges: edges}
 
     sources = find_starting_points(edges) |> Enum.uniq() |> sort_nodes()
-    dst = find_dst(edges)
-    walk(dst, edges, sources, MapSet.new())
+    walk(edges, sources, MapSet.new())
   end
 
-  defp walk(dst, edges, queue, done) do
+  defp new_worker(step, time) do
+    %{:current => step, :time_left => time}
+  end
+
+  defp do_work(worker, time) do
+    if worker[:current] == nil do
+      worker
+    else
+      %{:current => worker[:current], :time_left => worker[:time_left] - time}
+    end
+  end
+
+  defp give_step_to_worker(step, time_per_step) do
+    new_worker(step, time_per_step[step])
+  end
+
+  def part2(instructions, extra_step_time, worker_count) do
+    #IO.inspect "*************** BEGIN"
+    # Each tuple is edge {from, to}
+    edges = instructions
+      |> parse_instructions()
+    steps = edges |> Enum.flat_map(fn {f, t} -> [f, t] end) |> Enum.uniq()
+    #IO.inspect steps, label: "All steps"
+    time_per_step = steps |> Enum.map(fn s -> {s, time_for(s, extra_step_time)} end) |> Map.new()
+    #sources = find_starting_points(edges) |> Enum.uniq() |> sort_nodes()
+
+    worker_pool = for _ <- 1..worker_count, do: new_worker(nil, 0)
+
+    tick(-1, 1, time_per_step, worker_pool, edges, MapSet.new(), steps)
+  end
+
+  def tick(old_time, advance_by, time_per_step, worker_pool, edges, done, all_steps) do
+
+    #IO.inspect step_queue, label: "Q #{old_time}:"
+    if advance_by == 0, do: raise "time stands still"
+    this_time = old_time + advance_by
+    #IO.inspect this_time, label: "========== TIME"
+
+    #if old_time > 16, do: raise "stop"
+
+    # Check workers that are done
+    {new_workers, new_done} = worker_pool |> Enum.map_reduce(done, fn w, d ->
+      if w[:time_left] == 0 and not is_free(w) do
+        step = w[:current]
+        nd = MapSet.put(d, step)
+        w2 = new_worker(nil, 0)
+        {w2, nd}
+      else
+        {w, d}
+      end
+    end)
+
+    #IO.inspect new_workers, label: "workers after checking done"
+    #IO.inspect new_done, label: "done after checking done"
+
+    #if this_time > 20, do: raise "?????????"
+
+    if all_done(new_done, all_steps) do
+      this_time #TODO: Or old??
+    else
+
+
+      # 1. Give work to free workers
+      new_workers = assign_work_to_workers(edges, new_done, new_workers, time_per_step)
+
+      #IO.inspect new_workers, label: "workers after giving work"
+      #IO.inspect new_done, label: "done after giving work"
+
+      # 2. Let workers work
+      # {new_workers, new_done} = let_workers_work(new_workers, new_done)
+      new_workers = let_workers_work(new_workers)
+
+      # Give more work to now free workers!?
+
+      #IO.inspect new_workers, label: "workers after work"
+      #IO.inspect new_done, label: "done after work"
+
+      tick(this_time, advance_by, time_per_step, new_workers, edges, new_done, all_steps)
+    end
+  end
+
+  defp assign_work_to_workers(edges, done, workers, time_per_step) do
+    free_count = workers |> Enum.count(&is_free/1)
+    #IO.inspect free_count, label: "free workers"
+    pieces_of_work = take_from_queue(edges, done, workers, free_count)
+    #IO.inspect pieces_of_work, label: "pieces_of_work"
+
+    {busy_workers, _} = workers |> Enum.map_reduce(pieces_of_work, fn w, pieces ->
+      if is_free(w) and length(pieces) > 0 do
+        [piece | rest] = pieces
+        w2 = give_step_to_worker(piece, time_per_step)
+        {w2, rest}
+      else
+        {w, pieces}
+      end
+    end)
+    busy_workers
+  end
+
+  defp take_from_queue(edges, done, workers, count) do
+    calculate_queue(edges, done, workers) |> Enum.take(count)
+  end
+
+  defp calculate_queue(edges, done, workers) do
+    queue = all_next_nodes_given_done(edges, done) |> Enum.uniq() |> sort_nodes()
+    #IO.inspect queue, label: "Queue"
+    being_worked_on = workers |> Enum.reject(&is_free/1) |> Enum.map(fn w -> w[:current] end)
+    queue -- being_worked_on
+  end
+
+  defp let_workers_work(workers) do
+    workers |> Enum.map(fn w -> do_work(w, 1) end)
+  end
+
+  defp all_done(done_ms, steps_list) do
+    Enum.all?(steps_list, fn s -> MapSet.member?(done_ms, s) end)
+  end
+
+  defp is_free(worker) do
+    worker[:current] == nil
+  end
+
+  defp time_for(s, extra) do
+    (hd to_charlist(s)) - 64 + extra
+  end
+
+  defp walk(edges, queue, done) do
     if length(queue) == 0 do
       # Nothing more in the queue, so we're done
       ""
@@ -23,8 +146,18 @@ defmodule Day7 do
       # Find all next nodes, sort the new queue
       new_q = rest ++ all_next_nodes(nxt, edges, new_done) |> sort_nodes()
       # Emit 'nxt' and recurse
-      nxt <> walk(dst, edges, new_q, new_done)
+      nxt <> walk(edges, new_q, new_done)
     end
+  end
+
+  def all_next_nodes_given_done(edges, done) do
+    starting_points = find_starting_points(edges)
+    next_nodes = edges
+      |> Enum.filter(fn {_, t} -> reachable(t, edges, done) end)
+      |> Enum.map(fn {_, t} -> t end)
+    next_nodes ++ starting_points
+      |> Enum.uniq()
+      |> Enum.reject(fn n -> MapSet.member?(done, n) end)
   end
 
   defp all_next_nodes(node, edges, done) do
@@ -52,12 +185,6 @@ defmodule Day7 do
     edges
       |> Enum.filter(fn {f, _} -> not MapSet.member?(all_destinations, f) end)
       |> Enum.map(fn {f, _} -> f end)
-  end
-
-  defp find_dst(edges) do
-    all_from = edges |> Enum.map(fn {f, _} -> f end) |> MapSet.new()
-    {_, to} = Enum.find(edges, fn {_, t} -> not MapSet.member?(all_from, t) end)
-    to
   end
 
   defp parse_instructions(instructions) do
--- day7/test/day7_test.exs
+++ day7/test/day7_test.exs
@@ -17,6 +17,49 @@ defmodule Day7Test do
 
   test "part1" do
     instructions = Utils.readlines("input")
-    assert Day7.part1(instructions) == nil
+    assert Day7.part1(instructions) == "BFKEGNOVATIHXYZRMCJDLSUPWQ"
+  end
+
+  test "part 2 base case 1" do
+    instructions = [
+      "Step C must be finished before step A can begin."
+    ]
+    assert Day7.part2(instructions, 0, 1) == 4
+    assert Day7.part2(instructions, 0, 2) == 4
+  end
+
+  test "part 2 base case 2" do
+    instructions = [
+      "Step C must be finished before step A can begin.",
+      "Step B must be finished before step A can begin."
+    ]
+    assert Day7.part2(instructions, 0, 1) == 6 # BBCCCA
+    assert Day7.part2(instructions, 0, 2) == 4 # (B/C)(B/C)(./C)(A)
+  end
+
+  test "part 2 examples" do
+    instructions = [
+      "Step C must be finished before step A can begin.",
+      "Step C must be finished before step F can begin.",
+      "Step A must be finished before step B can begin.",
+      "Step A must be finished before step D can begin.",
+      "Step B must be finished before step E can begin.",
+      "Step D must be finished before step E can begin.",
+      "Step F must be finished before step E can begin."
+    ]
+    assert Day7.part2(instructions, 0, 2) == 15
+  end
+
+  test "part2" do
+    instructions = Utils.readlines("input")
+    assert Day7.part2(instructions, 60, 5) == 1020
+  end
+
+  test "all_next_nodes_given_done" do
+    assert Day7.all_next_nodes_given_done([{"A", "B"}], MapSet.new()) == ["A"]
+    assert Day7.all_next_nodes_given_done([{"A", "B"}], MapSet.new(["A"])) == ["B"]
+
+    assert Day7.all_next_nodes_given_done([{"B", "A"}, {"C", "A"}], MapSet.new(["B"])) == ["C"]
+    assert Day7.all_next_nodes_given_done([{"B", "A"}, {"C", "A"}], MapSet.new(["B", "C"])) == ["A"]
   end
 end
