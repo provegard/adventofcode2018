231a2f453065deebd299c15da86bf6e9252f7a9c
Per Rovegård <per@rovegard.se>
2018-12-12 10:25:25 +0100

    Day 12, part 2

--- day12_js/day12.js
+++ day12_js/day12.js
@@ -1,11 +1,11 @@
 const utils = require("../utils_js/utils");
 const Iterator = utils.Iterator;
 
-function sumPotsWithPlants(stateMap) {
+function sumPotsWithPlants(stateMap, offset = 0) {
     const indexes = Object.keys(stateMap).map((k) => parseInt(k));
     return indexes.reduce((sum, i) => {
         const pot = stateMap[i];
-        return sum + (pot === "#" ? i : 0)
+        return sum + (pot === "#" ? (i + offset) : 0)
     }, 0);
 }
 
@@ -28,7 +28,7 @@ function applyRules(stateMap, rules) {
     const useThese = Array.from(utils.range(minIdx - 2, maxIdx + 3));
     return useThese.reduce((newMap, i) => {
         const state = stateAt(stateMap, i);
-        newMap[i] = rules.has(state) ? "#" : ".";
+        if (rules.has(state)) newMap[i] = "#"; // sparse map
         return newMap;
     }, {})
 }
@@ -38,11 +38,37 @@ function stateToString(stateMap) {
     return indexes.map((i) => stateMap[i] || ".").join("");
 }
 
-function run(stateMap, rules, iterations) {
-    if (iterations > 0) {
-        return run(applyRules(stateMap, rules), rules, iterations - 1);
-    } else {
-        return stateMap;
+function indexOfFirstPot(stateMap) {
+    const indexes = Object.keys(stateMap).map((k) => parseInt(k)).sort((a, b) => a - b);
+    return indexes.find((i) => stateMap[i] === "#");
+}
+
+function stateDesc(stateMap) {
+    const indexes = Object.keys(stateMap).map((k) => parseInt(k)).sort((a, b) => a - b);
+    const firstPot = indexes.find((i) => stateMap[i] === "#");
+    const lastPot = indexes.reverse().find((i) => stateMap[i] === "#");
+    const str = Array.from(utils.range(firstPot, lastPot + 1)).map((i) => stateMap[i] || ".").join("");
+    return {firstPot, str};
+}
+
+function* generate(stateMap, rules) {
+    seen = {};
+    let iter = 0;
+    while (true) {
+        let {firstPot, str} = stateDesc(stateMap);
+        if (seen[str] !== undefined) {
+            const old = seen[str];
+            // seen -194, -196
+            // same pattern at minIdx that was previously at old
+            const shifted = firstPot - old.firstPot; // negative left, positive right
+            // pattern starts to repeat
+            // 2 steps to the left
+            throw {iter, shifted};
+        }
+        seen[str] = {iter, firstPot};
+        stateMap = applyRules(stateMap, rules);
+        iter++;
+        yield stateMap;
     }
 }
 
@@ -61,10 +87,32 @@ function parse(lines) {
     return {rules, initialState};
 }
 
+function run(initialState, rules, iterations) {
+    const states = generate(initialState, rules);
+    let idx = 0;
+    let state;
+    for (state of states) {
+        if (++idx === iterations) break;
+    }
+    return state;
+}
+
 function part1(lines, iterations = 20) {
+    lines = lines.concat();
     const {rules, initialState} = parse(lines);
-    const state = run(initialState, rules, iterations);
-    return sumPotsWithPlants(state);
+    try {
+        const state = run(initialState, rules, iterations);
+        return sumPotsWithPlants(state);
+    } catch (e) {
+        // At iteration iter, the pattern repeats by shifting
+        // 'shifted' steps on each iteration.
+        // Thus: run (iter-1) iterations
+        const iterBeforeRepeat = e.iter - 1;
+        const state = run(initialState, rules, iterBeforeRepeat);
+        const iterLeft = iterations - iterBeforeRepeat;
+        const totalShift = iterLeft * e.shifted;
+        return sumPotsWithPlants(state, totalShift);
+    }
 }
 
 function part2() {
--- day12_js/part2.test.js
+++ day12_js/part2.test.js
@@ -2,8 +2,11 @@ const utils = require("../utils_js/utils");
 const day12 = require("./day12");
 
 describe("part2", () => {
-    it("works for the input", () => {
-        //const lines = utils.readLines("input");
-        //expect(day12.part1(lines, 50000000000)).toBe(-1);
+    it("works for the input 1", () => {
+        const lines = utils.readLines("input");
+        expect(day12.part1(lines, 200)).toBe(9684);
+        expect(day12.part1(lines, 400)).toBe(17684);
+        expect(day12.part1(lines, 1000)).toBe(41684);
+        expect(day12.part1(lines, 50000000000)).toBe(2000000001684);
     });
 });
