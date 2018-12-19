777b25f3bdfb40ff130e618428e783bcc910e37f
Per Rovegård <per@rovegard.se>
2018-12-19 05:56:58 +0100

    Day 19 skeleton

---
+++ day19_js/part1.test.js
@@ -0,0 +1,10 @@
+const utils = require("../utils_js/utils");
+const Day19 = require("./day19");
+
+describe("part1", () => {
+    it("works for the example", () => {
+    });
+
+    it("works for the input", () => {
+    });
+});
---
+++ day19_js/part2.test.js
@@ -0,0 +1,7 @@
+const utils = require("../utils_js/utils");
+const Day19 = require("./day19");
+
+describe("part2", () => {
+    it("works for the input", () => {
+    });
+});
