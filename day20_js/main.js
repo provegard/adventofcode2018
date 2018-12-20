96941effb41a12164032eda612d97d1ca728b47e
Per Rovegård <per@rovegard.se>
2018-12-20 06:41:39 +0100

    Day 20, part 1

--- day20_js/main.js
+++ day20_js/main.js
@@ -1,11 +1,79 @@
 const utils = require("../utils_js/utils");
 const _ = require("lodash");
 
+const key = (x, y) => x + "," + y;
 
-function part1(lines) {
+function subPaths(rest) {
+    let balance = 1;
+    let i = 0;
+    const paths = [];
+    let curPath = "";
+    while (balance > 0) {
+        const next = rest[i];
+        if (!next) throw new Error("EOS");
+        if (next === "|" && balance === 1) {
+            // only count branches on the top level
+            paths.push(curPath);
+            curPath = "";
+        } else {
+            if (next === "(") balance++;
+            if (next === ")") balance--;
+            if (balance > 0) curPath += next; // don't include the last closing )
+        }
+        i++
+    }
+    if (curPath) paths.push(curPath);
+    const remainder = rest.substr(i);
+    return paths.map((p) => p + remainder);
+}
+
+function walk(x, y, distance_walked, steps, map) {
+    while (true) {
+        const next = steps[0];
+        const rest = steps.slice(1);
+        steps = rest;
+        const k = key(x, y);
+        const best = map[k];
+        if (best !== undefined) {
+            if (best < distance_walked) {
+                // ignore longer paths that lead here
+                distance_walked = best;
+            } else {
+                map[k] = distance_walked;
+            }
+        } else {
+            map[k] = distance_walked;
+        }
+        switch (next) {
+            case "$":
+                return map;
+            case "(":
+                // branch
+                for (const path of subPaths(rest)) {
+                    walk(x, y, distance_walked, path, map);
+                }
+                return map;
+            case "W": x++; break;
+            case "E": x--; break;
+            case "N": y--; break;
+            case "S": y++; break;
+            default:
+                throw new Error("Unhandled: " + next);
+        }
+        distance_walked++;    
+    }
+}
+
+function buildFacility(regex) {
+    return walk(0, 0, 0, regex.slice(1), {});
+}
+
+function part1(regex) {
+    const f = buildFacility(regex);
+    return _.max(_.values(f));
 }
 
 function part2(lines) {
 }
 
-module.exports = { part1, part2 };
+module.exports = { part1, part2, subPaths };
--- day20_js/part1.test.js
+++ day20_js/part1.test.js
@@ -2,9 +2,23 @@ const utils = require("../utils_js/utils");
 const main = require("./main");
 
 describe("part1", () => {
-    it("works for the example", () => {
+    it("finds sub paths", () => {
+        expect(main.subPaths("NEEE|SSE(EE|N))W")).toEqual([
+            "NEEEW",
+            "SSE(EE|N)W"
+        ]);
+    });
+    it("works for the examples", () => {
+        expect(main.part1("^WNE$")).toBe(3);
+        expect(main.part1("^ENWWW(NEEE|SSE(EE|N))$")).toBe(10);
+        expect(main.part1("^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$")).toBe(18);
+        expect(main.part1("^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$")).toBe(23);
+        expect(main.part1("^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$")).toBe(31);
     });
 
     it("works for the input", () => {
+        const lines = utils.readLines("input");
+        const regex = lines[0];
+        expect(main.part1(regex)).toBe(3476);
     });
 });
