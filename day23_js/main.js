a53ddad8437f8e002c987d26cfc0a328d4de3924
Per Rovegård <per@rovegard.se>
2018-12-23 23:50:45 +0100

    Day 23 part 2 WIP

--- day23_js/main.js
+++ day23_js/main.js
@@ -12,6 +12,16 @@ class NanoBot {
     manhattan(bot) {
         return Math.abs(this.x - bot.x) + Math.abs(this.y - bot.y) + Math.abs(this.z - bot.z);
     }
+
+    inRange(pos) {
+        return this.manhattan(pos) <= this.r;
+    }
+    
+    overlaps(bot) {
+        const dist = this.manhattan(bot);
+        const radiiSum = this.r + bot.r;
+        return radiiSum >= dist;
+    }
 }
 NanoBot.parse = (line) => {
     // pos=<0,0,0>, r=4
@@ -31,7 +41,64 @@ function part1(lines) {
     return inRange.length;
 }
 
-function part2(lines) {
+function mean(numbers) {
+    const sum = _.sum(numbers);
+    return sum / numbers.length;
 }
 
-module.exports = { part1, part2 };
+function median(numbers) {
+    const nn = numbers.concat();
+    nn.sort();
+    if (nn.length % 2 === 0) {
+        const a = nn[nn.length / 2 - 1];
+        const b = nn[nn.length / 2];
+        return (a + b) / 2;
+    } else {
+        return nn[(nn.length - 1) / 2];
+    }
+}
+
+function centerOfMass(bots) {
+    const mass = (bot) => bot.r;
+    const center = (component) => {
+        const t = bots.reduce((sum, b) => sum + b[component] * mass(b), 0);
+        const n = _.sumBy(bots, mass);
+        return t / n;
+    };
+    const cx = Math.round(center("x"));
+    const cy = Math.round(center("y"));
+    const cz = Math.round(center("z"));
+    return new NanoBot(cx, cy, cz, 0);
+}
+
+function part2(lines) {
+    const myPos = new NanoBot(0, 0, 0, 0);
+    let bots = lines.map(NanoBot.parse);
+
+    const groups = [];
+    while (bots.length > 0) {
+        const bot = bots.shift();
+        const group = [bot];
+        for (const b of bots) {
+            if (group.every((x) => x.overlaps(b))) {
+                group.push(b);
+            }
+        }
+
+        groups.push(group);
+
+        bots = bots.filter((b) => group.indexOf(b) < 0);
+    }
+
+    const largestGroupSize = _.maxBy(groups, (g) => g.length).length;
+    const largestGroups = groups.filter((g) => g.length === largestGroupSize);
+
+    const positions = largestGroups.map((g) => {
+        return centerOfMass(g);
+    });
+    if (positions.length === 0) throw new Error("no positions");
+    const x = _.minBy(positions, (p) => myPos.manhattan(p));
+    return myPos.manhattan(x);
+}
+
+module.exports = { part1, part2, NanoBot };
--- day23_js/part2.test.js
+++ day23_js/part2.test.js
@@ -4,11 +4,12 @@ const main = require("./main");
 describe("part2", () => {
     it("works for the example", () => {
         const lines = utils.readLines("example2");
-        expect(main.part2(lines)).toEqual(new main.Pos(12, 12, 12));
+        expect(main.part2(lines)).toBe(36);
     });
 
-    it("works for the input", () => {
+    fit("works for the input", () => {
         const lines = utils.readLines("input");
+        // 38630826 is too low
         expect(main.part2(lines)).toEqual(null);
     });
 });
