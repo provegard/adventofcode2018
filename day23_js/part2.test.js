2abe4cb3f477fd0e4e8dae95345a704b495538c1
Per Rovegård <per@rovegard.se>
2018-12-23 08:53:30 +0100

    WIP

--- day23_js/part2.test.js
+++ day23_js/part2.test.js
@@ -2,8 +2,13 @@ const utils = require("../utils_js/utils");
 const main = require("./main");
 
 describe("part2", () => {
+    it("works for the example", () => {
+        const lines = utils.readLines("example2");
+        expect(main.part2(lines)).toEqual(new main.Pos(12, 12, 12));
+    });
+
     it("works for the input", () => {
-        //const lines = utils.readLines("input");
-        //expect(main.part2(lines)).toBe(0);
+        const lines = utils.readLines("input");
+        expect(main.part2(lines)).toEqual(null);
     });
 });
